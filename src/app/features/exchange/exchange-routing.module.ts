f32064790324943ce3106ba4dbea36d3107055d3
steventeosoft <steven.teo.soft@gmail.com>
2024-09-16 20:36:09 +0900

    exchange step1 and fixed style

--- src/app/features/exchange/exchange-routing.module.ts
+++ src/app/features/exchange/exchange-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'step1',
     loadChildren: () =>
       import('./exchange-list/exchange-list.module').then(
         (m) => m.ExchangeListModule
--- src/app/shared/components/asset-selector/asset-selector.component.ts
+++ src/app/shared/components/asset-selector/asset-selector.component.ts
@@ -70,6 +70,7 @@ export class AssetSelectorComponent implements OnChanges {
     if (changes['isLoading']) {
       if (!this.spinner) return;
 
+      // this.swapFormService.outputControl.patchValue({ toAmount: null });
       this.spinner.nativeElement.style.display = changes['isLoading'].currentValue ? 'flex' : 'none';
     }
   }
