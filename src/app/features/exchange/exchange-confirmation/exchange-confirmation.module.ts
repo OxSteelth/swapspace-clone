a3e50ba5001ed79c36f79b93585928d6570dd5f5
steventeosoft <steven.teo.soft@gmail.com>
2024-09-30 21:46:10 +0900

    removed unused code

--- src/app/features/exchange/exchange-confirmation/exchange-confirmation.module.ts
+++ src/app/features/exchange/exchange-confirmation/exchange-confirmation.module.ts
@@ -3,14 +3,13 @@ import { RouterModule } from '@angular/router';
 import { ExchangeConfirmationComponent } from './exchange-confirmation.component';
 import { SharedModule } from '../../../shared/shared.module';
 import { ConfirmationCardComponent } from './components/confirmation-card/confirmation-card.component';
-import { ExchangeConfirmationViewModel } from './viewmodel/exchange-confirmation.viewmodel.';
 
 @NgModule({
   declarations: [
     ExchangeConfirmationComponent,
     ConfirmationCardComponent,
   ],
-  providers: [ExchangeConfirmationViewModel],
+  providers: [],
   imports: [
     SharedModule,
     RouterModule.forChild([
