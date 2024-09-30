f721053035fabb61bffd621b8de54ac7a2db166e
OxSteelth <oxsteelth@gmail.com>
2024-09-30 15:31:12 +0900

    refactor: dev api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const ENVIRONMENT = {
   production: false,
 
-  apiBaseUrl: 'http://127.0.0.1:3000/api/v2',
+  apiBaseUrl: 'https://oneany-api.vercel.app/api/v2',
 
   iconBaseUrl: 'https://storage.swapspace.co',
 };
